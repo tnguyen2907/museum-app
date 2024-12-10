487d71717dbbbf7f8ff2b7e3eca61642de58ff19
Trung Nguyen <tnguyen2@oberlin.edu>
2024-12-10 16:22:36 -0500

    add url

--- backend/index.js
+++ backend/index.js
@@ -152,5 +152,5 @@ app.get('/get_unique_periods', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://${process.env.DB_HOST}:${PORT}`);
 });
